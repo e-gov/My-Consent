bc0c4aa0c403c18764a458a18df46ea5a711c9e4
Priit Parmakson <priit.parmakson@gmail.com>
2018-11-30 17:19:43 +0200

    +

--- public/js/index.js
+++ public/js/index.js
@@ -5,9 +5,58 @@ const Teateala = document.getElementById('Teateala');
 
 function alusta() {
   $('#Allkirjastan').click(() => {
-    sign();
+    var toendiKeha = koostaToendiKeha('PRIIT', 'Allkiri');
+    koostaNousolekutoend(toendiKeha);
+    allkirjasta();
     // Valjastusala.textContent = 'OK';
   });
+
+  function koostaToendiKeha(andja, allkiri) {
+    return {
+      "kellele": $('Kellele').val(),
+      "milleks": $('Milleks').val(),
+      "mis ajani": $('MisAjani').val(),
+      "nõusoleku andis": andja,
+      "nõusoleku andja allkiri": allkiri,
+      "tõendi väljastas": "Volli-POC",
+      "tõend väljastatud": new Date().toUTCString()
+    }
+  }
+
+  function koostaNousolekutoend(toendiKeha) {
+    var header = {
+      "alg": "HS256",
+      "typ": "JWT"
+    }
+
+    var secret = 'Tõendi allkirja saladus';
+
+    function base64url(source) {
+      // Encode in classical base64
+      encodedSource = CryptoJS.enc.Base64.stringify(source);
+
+      // Remove padding equal characters
+      encodedSource = encodedSource.replace(/=+$/, '');
+
+      // Replace characters according to base64url specifications
+      encodedSource = encodedSource.replace(/\+/g, '-');
+      encodedSource = encodedSource.replace(/\//g, '_');
+
+      return encodedSource;
+    }
+
+    var stringifiedHeader = CryptoJS.enc.Utf8.parse(JSON.stringify(header));
+    var encodedHeader = base64url(stringifiedHeader);
+
+    var stringifiedData = CryptoJS.enc.Utf8.parse(JSON.stringify(toendiKeha));
+    var encodedData = base64url(stringifiedData);
+
+    var signature = encodedHeader + "." + encodedData;
+    signature = CryptoJS.HmacSHA256(signature, secret);
+    signature = base64url(signature);
+
+    return encodedData + "." + signature;
+  }
 
   function log_text(s) {
     var d = document.createElement("div");
@@ -19,7 +68,7 @@ function alusta() {
     window.hwcrypto.debug().then(function (response) { log_text("Debug: " + response); });
   }
 
-  function sign() {
+  function allkirjasta() {
     // Clear log
     document.getElementById('Valjastusala').innerHTML = '';
     // Timestamp
@@ -31,7 +80,7 @@ function alusta() {
 
     var hashtype = 'SHA-256';
     var hash = '413140d54372f9baf481d4c54e2d5c7bcf28fd6087000280e07976121dd54af2';
-    log_text("Signing " + hashtype + ": " + hash);
+    // log_text("Signing " + hashtype + ": " + hash);
 
     // debug
     window.hwcrypto.debug().then(function (response) {
