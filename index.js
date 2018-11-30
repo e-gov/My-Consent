117f52bcaf78a906efae540a0898afe0f0447bfc
Priit Parmakson <priit.parmakson@gmail.com>
2018-11-30 18:07:54 +0200

    +

--- index.js
+++ index.js
@@ -16,11 +16,17 @@ const crypto = require('crypto');
 /* Määra räsialgoritm - SHA256 */
 const HASH_ALGO = 'sha256';
 
+/* JWT töötlemise teek */
+const jsonwebtoken = require('jsonwebtoken');
+
 /* Veebiserveri ettevalmistamine */
 const app = express();
 /* Kui Heroku keskkonnamuutujas ei ole määratud teisiti,
  siis kasutatakse porti 80. */
 app.set('port', (process.env.PORT || 80));
+
+/* HTTP päringu keha töötlemise teek */
+const bodyParser = require('body-parser');
 
 /* Sea juurkaust, millest serveeritakse sirvikusse ressursse
  vt http://expressjs.com/en/starter/static-files.html 
@@ -33,6 +39,9 @@ app.set('views', __dirname + '/views');
 /* Määra kasutatav mallimootor */
 app.set('view engine', 'ejs');
 
+/* Loo body-parser töötlema application/json päringuid */
+var jsonParser = bodyParser.json();
+
 /**
  *  Järgnevad marsruuteri töötlusreeglid
  */
@@ -43,6 +52,30 @@ app.set('view engine', 'ejs');
  */
 app.get('/', function (req, res) {
   res.render('pages/index');
+});
+
+/**
+ * Tõendi moodustamine
+ * AJAX-otspunkt,
+ * võtab sirvikust tõendi keha, tagastab tõendi.
+ */
+app.get('/getJWT', jsonParser, function (req, res) {
+
+  if (!req.body) {
+    return res.status(500).json({ error: 'Tõendi keha ei tulnud päringus'});
+  }
+
+  var jwt = jsonwebtoken.sign(
+    req.body.payload,
+    'Tõendi allkirja saladus',
+    {
+      "algorithm": "HS256",
+      "issuer": "Volli-POC",
+      "subject": "Nõusolekutõend"
+    });
+
+  res.status(200).json({ jwt: jwt});
+
 });
 
 /**
--- public/js/index.js
+++ public/js/index.js
@@ -5,9 +5,24 @@ const Teateala = document.getElementById('Teateala');
 
 function alusta() {
   $('#Allkirjastan').click(() => {
+    // allkirjasta();
     var toendiKeha = koostaToendiKeha('PRIIT', 'Allkiri');
-    koostaNousolekutoend(toendiKeha);
-    allkirjasta();
+
+    // Tee AJAX-pöördumine serveri poolele tõendi moodustamiseks.
+    var postDeferred = $.post(
+      'https://volli-poc.herokuapp.com/getJWT',
+     toendiKeha);
+    postDeferred.done(function(data, status) {
+      console.log('salvestaTekst: POST vastus: data (töölehe tulemus): ' + data.result);
+      console.log('salvestaTekst: POST vastus: status: ' + status);
+      if (status !== 'success') {
+        logi_text('Tõendi moodustamine ebaõnnestus. :(');
+        return
+      }
+      logi_text('Tõend:');
+      logi_text(data);
+    });
+
     // Valjastusala.textContent = 'OK';
   });
 
@@ -21,41 +36,6 @@ function alusta() {
       "tõendi väljastas": "Volli-POC",
       "tõend väljastatud": new Date().toUTCString()
     }
-  }
-
-  function koostaNousolekutoend(toendiKeha) {
-    var header = {
-      "alg": "HS256",
-      "typ": "JWT"
-    }
-
-    var secret = 'Tõendi allkirja saladus';
-
-    function base64url(source) {
-      // Encode in classical base64
-      encodedSource = CryptoJS.enc.Base64.stringify(source);
-
-      // Remove padding equal characters
-      encodedSource = encodedSource.replace(/=+$/, '');
-
-      // Replace characters according to base64url specifications
-      encodedSource = encodedSource.replace(/\+/g, '-');
-      encodedSource = encodedSource.replace(/\//g, '_');
-
-      return encodedSource;
-    }
-
-    var stringifiedHeader = CryptoJS.enc.Utf8.parse(JSON.stringify(header));
-    var encodedHeader = base64url(stringifiedHeader);
-
-    var stringifiedData = CryptoJS.enc.Utf8.parse(JSON.stringify(toendiKeha));
-    var encodedData = base64url(stringifiedData);
-
-    var signature = encodedHeader + "." + encodedData;
-    signature = CryptoJS.HmacSHA256(signature, secret);
-    signature = base64url(signature);
-
-    return encodedData + "." + signature;
   }
 
   function log_text(s) {
