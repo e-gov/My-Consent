d1ae99aaabf4c6af80282a079b5364ce831777f3
Priit Parmakson <priit.parmakson@gmail.com>
2018-12-18 23:03:39 +0200

    +

--- index.js
+++ index.js
@@ -87,26 +87,26 @@ app.post('/decodeCert', jsonParser, function (req, res) {
 
   // Koosta objekt huvipakkuvatest elementidest
   var h = new Object;
-  
+
   h['serialNumber'] = ab2str(dc.serialNumber.valueBlock.valueHex);
-  
+
   var issuer = '';
   dc.issuer.typesAndValues.forEach((e) => {
-      issuer = issuer + 
-      ( (issuer === '') ? '' : ', ') + 
+    issuer = issuer +
+      ((issuer === '') ? '' : ', ') +
       e.value.valueBlock.value;
   });
-  
+
   var subject = '';
   dc.issuer.typesAndValues.forEach((e) => {
-      subject = subject + 
-      ( (subject === '') ? '' : ', ') + 
+    subject = subject +
+      ((subject === '') ? '' : ', ') +
       e.value.valueBlock.value;
   });
-  
+
   h['issuer'] = issuer;
   h['subject'] = subject;
-  
+
   console.log(JSON.stringify(h));
   res.status(200)
     .json(
@@ -147,26 +147,33 @@ app.post('/getJWT', jsonParser, function (req, res) {
 
 // babel-polyfill needs to be loaded for pkijs
 // It uses webcrypto which needs browser shims
-require('babel-polyfill')
+require('babel-polyfill');
 
 function decodeCert(cert) {
-    if(typeof cert !== 'string') {
-        throw new Error('Expected PEM as string')
-    }
+  if (typeof cert !== 'string') {
+    throw new Error('Expected PEM as string')
+  }
 
-    // Load certificate in PEM encoding (base64 encoded DER)
-    const b64 = cert.replace(/(-----(BEGIN|END) CERTIFICATE-----|[\n\r])/g, '')
+  // Load certificate in PEM encoding (base64 encoded DER)
+  const b64 = cert.replace(/(-----(BEGIN|END) CERTIFICATE-----|[\n\r])/g, '')
 
-    // Now that we have decoded the cert it's now in DER-encoding
-    const der = Buffer(b64, 'base64')
+  // Now that we have decoded the cert it's now in DER-encoding
+  const der = Buffer(b64, 'base64')
 
-    // And massage the cert into a BER encoded one
-    const ber = new Uint8Array(der).buffer
+  // And massage the cert into a BER encoded one
+  const ber = new Uint8Array(der).buffer
 
-    // And now Asn1js can decode things \o/
-    const asn1 = Asn1js.fromBER(ber)
+  // And now Asn1js can decode things \o/
+  const asn1 = Asn1js.fromBER(ber)
 
-    return new Pkijs.Certificate({ schema: asn1.result })
+  return new Pkijs.Certificate({ schema: asn1.result })
+}
+
+/* ArrayBuffer to String */
+function ab2str(hashBuffer) {
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
+  return hashHex;
 }
 
 /**
