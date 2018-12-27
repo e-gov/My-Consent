277b80e02806bb513af4d38fd79dfb66ed35ca2a
Priit Parmakson <priit.parmakson@ria.ee>
2018-12-27 22:08:29 +0200

    +

--- minhttps.js
+++ minhttps.js
@@ -13,6 +13,16 @@ const express = require('express');
 
 const app = express();
 
+// Sea juurkaust, millest serveeritakse sirvikusse ressursse
+app.use(express.static(__dirname + '/public'));
+
+// Sea rakenduse vaadete (kasutajale esitatavate HTML-mallide) kaust
+app.set('views', __dirname + '/views');
+
+// Määra kasutatav mallimootor
+app.set('view engine', 'ejs');
+
+
 // Esilehe kuvamine
 app.get('/', function (req, res) {
   console.log('avaleht');
@@ -23,15 +33,17 @@ app.get('/', function (req, res) {
 var HTTPS_S_options = {
   key: fs.readFileSync(
     path.join(__dirname, 'keys',
-      'isetehtud.key'), 'utf8'),
+      'https-server.key'), 'utf8'),
   cert: fs.readFileSync(
     path.join(__dirname, 'keys',
-      'isetehtud.cert'), 'utf8'),
+      'https-server.cert'), 'utf8'),
   ca: [
     fs.readFileSync(path.join(__dirname, 'keys',
-      'ESTEID-SK_2015.pem.crt'), 'utf8'),
-    fs.readFileSync(path.join(__dirname, 'keys',
-      'EE_Certification_Centre_Root_CA.pem.crt'), 'utf8')
+      'ca.cert'), 'utf8')
+//    fs.readFileSync(path.join(__dirname, 'keys',
+//      'ESTEID-SK_2015.pem.crt'), 'utf8'),
+//    fs.readFileSync(path.join(__dirname, 'keys',
+//      'EE_Certification_Centre_Root_CA.pem.crt'), 'utf8')
   ],
   requestCert: false,
   rejectUnauthorized: false
@@ -40,6 +52,6 @@ var HTTPS_S_options = {
 var httpsServer = https.createServer(HTTPS_S_options, app);
 
 // Veebiserveri käivitamine 
-httpsServer.listen(5000, () => {
+httpsServer.listen(8000, () => {
   console.log('HTTPS server kuuldel pordil: ' + httpsServer.address().port);
 });
